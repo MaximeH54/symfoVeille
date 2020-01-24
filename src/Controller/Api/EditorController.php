d6517cdd7ce8ca62c0683ea63ffee76af28770ad
Maxime <maxime_hemmerle@orange.fr>
2020-01-24 14:14:17 +0100

    symfony (sorry not existing)

---
+++ src/Controller/Api/EditorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Editor;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class EditorController extends AbstractController
+{
+    /**
+     * @Route("/editor", name="editor")
+     */
+    public function index()
+    {
+				$editors = $this->getDoctrine()->getRepository(Editor::class)->findAll();
+
+				$response = [];
+
+				foreach ($editors as $editor) {
+					$response[] = [
+						'id' => $editor->getId(),
+						'name' => preg_replace('/\s+/', ' ', str_replace("\n", '', $editor->getName())),
+					];
+				}
+
+        return $this->json($response);
+    }
+		/**
+     * @Route("/editor/{id}", name="editor_detail", requirements={"id"="\d+"})
+     */
+		 public function editor($id)
+		 {
+			 $editor = $this->getDoctrine()->getRepository(Editor::class)->find($id);
+
+			 if (!$editor) {
+				 throw new NotFoundHttpException('Sorry not existing!');				 
+			 }
+
+			 $games = [];
+
+			 foreach ($editor->getGames() as $game) {
+				 $games[] = [
+					 'id' => $game->getId(),
+					 'name' => $game->getName(),
+				 ];
+			 }
+			 dump($games);
+			 $response = [
+				 'id' => $editor->getId(),
+				 'name' => $editor->getName(),
+				 'games' => $games,
+			 ];
+
+			 return $this->json($response);
+		 }
+}
